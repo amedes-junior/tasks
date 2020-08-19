98d359bfd16e6595b6248bc8f03ea527dfc1db3c
Amedes Júnior <amedes@gmail.com>
2020-08-19 10:37:16 -0300

    commit inicial

---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+import commonStyles from '../commonStyles'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export default props => {
+  return (
+    <View style={styles.container}>
+      <Text>{props.desc}</Text>
+      <Text>{props.estimateAt + ""}</Text>
+      <Text>{props.doneAt + ""}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10
+  }
+})
